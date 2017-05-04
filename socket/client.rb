b3288e3011fec358b78b8fe6281be43f97ea701f
pumpkin <zhenhuanlee@outlook.com>
2017-05-04 16:00:44 +0800

    fork and thread

--- socket/client.rb
+++ socket/client.rb
@@ -3,10 +3,13 @@ require 'pry'
 
 Socket.tcp('127.0.0.1', 4481) do |conn|
   loop do
+    Thread.new do
+      IO.select([conn])
+      puts conn.gets
+    end
+
     puts 'Please input sth:'
     sth = gets.chomp
     conn.puts(sth)
-    IO.select([conn])
-    puts conn.gets
   end
 end
--- socket/server.rb
+++ socket/server.rb
@@ -3,11 +3,16 @@ require 'pry'
 
 puts 'listening...'
 Socket.tcp_server_loop(4481) do |conn|
-  loop do
-    request = conn.gets
-    puts request
-    break if request.strip == 'exit'
-    conn.puts 'hi'
-    IO.select(nil, [conn], nil)
+  address = conn.remote_address
+  puts "#{address.ip_address}:#{address.ip_port} connected!"
+  fork do
+    loop do
+      puts 'waiting'
+      IO.select(nil, [conn], nil)
+      request = conn.gets
+      puts "#{address.ip_address}(#{address.ip_port}): " + request
+      exit if (request || '').strip == 'exit' || request.nil?
+      conn.puts 'hi'
+    end
   end
 end
