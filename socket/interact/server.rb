a8fff7ec818ce1a0bc82f29d4040a31a52c6ac27
pumpkin <zhenhuanlee@outlook.com>
2017-05-14 16:00:34 +0800

    ~

--- socket/interact/server.rb
+++ socket/interact/server.rb
@@ -1,23 +1,46 @@
+# 事件驱动
 require 'socket'
 require 'pry'
 
-CHUNKSIZE = 1024 * 16
+class Connection
+  CHUNKSIZE = 1024 * 16
+  attr_reader :client
+
+  def initialize(conn)
+    @client = conn
+  end
+
+  def respond(str)
+    @client.write_nonblock(str)
+  end
+
+  def data
+    @client.read_nonblock(CHUNKSIZE)
+  end
+
+  def addrinfo
+    addr = @client.remote_address
+    "#{addr.ip_address}:#{addr.ip_port}"
+  end
+
+end
+
+
 CRLF = "\r\n"
 @handles = {}
 socket = TCPServer.new(4481)
 
 loop do
-  conns = @handles.values
+  conns = @handles.values.map(&:client)
   reads, writes = IO.select(conns + [socket], conns)
   reads.each do |read|
     if read == socket 
-      conn = read.accept
-      add = conn.remote_address
-      puts "#{add.ip_address}:#{add.ip_port} connected."
-      @handles[conn.fileno] = conn
+      conn = Connection.new(read.accept)
+      puts conn.addrinfo + 'connected'
+      @handles[conn.client.fileno] = conn
     else
       begin
-        content = read.read_nonblock(CHUNKSIZE)
+        content = @handles[read.fileno].data
         puts content
       rescue Errno::EAGAIN
       rescue EOFError
@@ -27,5 +50,4 @@ loop do
 
   end
 end
-
 
